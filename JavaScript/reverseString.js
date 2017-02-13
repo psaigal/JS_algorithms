efde3ed3c3c4a4b7859a334aea3f6f4a30da531a
Priyanka Saigal <email": null>
2017-02-13 15:26:39 -0800

    First commit

---
+++ JavaScript/reverseString.js
@@ -0,0 +1,28 @@
+// Question: Reverse a String
+
+// Steps
+// 1. Function takes a string as an argument
+// 2. The last letter should equal the original first letter
+// 3. The first letter should equal the original last
+// 4. Continue this until you reach the middle of the string
+// 5. The output should be the string in reversed order
+
+// Examples
+// Input:
+// 1. "hello"
+// 2. "y"
+// 3. "http"
+// 4. "racecar"
+
+// Output:
+// 1. "olleh"
+// 2. "y"
+// 3. "ptth"
+// 4. "racecar"
+
+// IMPORTANT NOTE: JavaScript strings are immutable(cannot be changed). This is because strings are primitive values.
+
+
+
+
+
---
+++ JavaScript/verifyPrime.js
@@ -0,0 +1,52 @@
+// Question: Check if the number is a prime number
+
+// Steps
+// 1. Write a function that takes a number
+// 2. Verify if that number is a prime number
+// 3. Prime numbers are only divisble by 1 and themselves (like 7, 13, 31);
+// 4. If the number
+
+// // Input
+// 1. 4
+// 2. 3
+// 3. 11
+// 4. 1
+// 5. 0
+// 6. -2
+
+
+// // Output
+// 1. false
+// 2. true
+// 3. true
+// 4. false
+// 5. false
+// 6. false
+
+function isNumberPrime(number) {
+  var numbers = [2,3,4,5,6,7,8,9];
+  if (number <= 0 || number === 1 || number === 2 || number === 3 || number === 5 || number === 7) {
+    return true;
+  }
+  else if (number <= 9) {
+    return false;
+  }
+  else if (number > 9) {
+    for(var i = 0; i <= numbers.length-1; i++) {
+      if (number % numbers[i] === 0) {
+        return false;
+      }
+      else {
+        return true;
+      }
+    }
+  }
+}
+
+isNumberPrime(12);
+
+
+
+
+
+
