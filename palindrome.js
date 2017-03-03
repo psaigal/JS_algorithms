1105c5a079eca2b55be840003024e6b79601c4b6
Priyanka Saigal <email": null>
2017-03-03 11:29:32 -0800

    Add part of palindrome challenge

---
+++ palindrome.js
@@ -0,0 +1,28 @@
+function palindrome(str) {
+  // Good luck!
+  return true;
+}
+
+
+//Input: string
+//Output: boolean
+
+// Example: racecar
+// 1. Take string racecar
+// 2. Check if first and last letter match
+// 3. Continue until you reach middle of string, if the string is an odd number of character
+// 4. In this example, we would return true
+
+
+// Example: miooim
+// 1. Take string miooim
+// 2. Take string length/2. In this case 6/2 = 3
+// 3. Check if first and last letter match
+// 4. Continue until you reach middle of string, if the string is an odd number of character
+// 5. In this example, we would return true
+
+
+
+
+
+palindrome("eye");
