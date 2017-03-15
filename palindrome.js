b3f4bc1d1192ad77fa9812c5f4958578e9c4d34c
Priyanka Saigal <email": null>
2017-03-15 15:39:11 -0700

    Add comments

--- palindrome.js
+++ palindrome.js
@@ -1,20 +1,14 @@
-function palindrome(str) {
-  // Good luck!
-  return true;
-}
-
-
 //Input: string
 //Output: boolean
 
-// Example: racecar
+// Example: racecar(odd numer string)
 // 1. Take string racecar
 // 2. Check if first and last letter match
 // 3. Continue until you reach middle of string, if the string is an odd number of character
 // 4. In this example, we would return true
 
 
-// Example: miooim
+// Example: miooim(even number string)
 // 1. Take string miooim
 // 2. Take string length/2. In this case 6/2 = 3
 // 3. Check if first and last letter match
@@ -23,6 +17,10 @@ function palindrome(str) {
 
 
 
+function palindrome(str) {
+  // Good luck!
+  return true;
+}
 
 
 palindrome("eye");
