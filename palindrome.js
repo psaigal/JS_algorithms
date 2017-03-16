485e3d205179d63be6814d056c2329bade6ff2b9
Priyanka Saigal <email": null>
2017-03-15 17:23:12 -0700

    Add solution for palindrome

--- palindrome.js
+++ palindrome.js
@@ -1,14 +1,13 @@
-//Return true or false if a string is a palindrome. A palindrome is a string that is spelled the same way backwards and forwards.
+//Return true or false if a string is a palindrome. A palindrome is a string that is spelled the same way backwards and forwards. Spaces, punctuation, uppercase letters, and symbols must be removed.
 
 //Input: string
 //Output: boolean
 
 
-// Example: racecar(odd numer string)
-// 1. Take string racecar
-// 2. Check if first and last letter match
-// 3. Continue until you reach middle of string, if the string is an odd number of character
-// 4. In this example, we would return true
+// Example: racecar(odd numer string) 1. Take string racecar 2. Check if first
+// and last letter match 3. Continue until you reach middle of string, if the
+// string is an odd number of character 4. In this example, we would return
+// true
 
 
 // Example: miooim(even number string)
@@ -21,9 +20,23 @@
 
 
 function palindrome(str) {
-  // Good luck!
+  var newStr = str.replace(/[,.()\-\_ ]+/g, "").trim().toLowerCase();
+  var strArray = newStr.split("");
+  var j = newStr.length-1;
+  for (var i = 0; i <= newStr.length/2; i++) {
+    var start = strArray[i];
+    var end = strArray[j];
+    if (start != end) {
+      console.log(i + " i");
+      return false;
+    }
+    j--;
+  }
   return true;
 }
+
+
+palindrome("0_0 (: /-\ :) 0-0");
 
 
 palindrome("eye");
