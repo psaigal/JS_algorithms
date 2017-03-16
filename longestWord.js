5cf7be85272ad59ab09bf87a0c97b6c3e2a51f16
psaigal <psaigal48@gmail.com>
2017-03-15 17:53:56 -0700

    Add longestWord algo

---
+++ longestWord.js
@@ -0,0 +1,12 @@
+//Find longest string in the sentence
+// Input: string
+// Output: string
+
+
+
+
+function findLongestWord(str) {
+  return str.length;
+}
+
+findLongestWord("The quick brown fox jumped over the lazy dog");
